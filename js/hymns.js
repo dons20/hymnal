816fae51bc979b0d4abdb970186f963ab5115e8a
Keno Clayton <keno.clayton15@gmail.com>
2018-08-17 11:53:17 -0500

    Removed Jquery dependency, included ES6 features

--- js/hymns.js
+++ js/hymns.js
@@ -1,208 +1,249 @@
-'use strict';
+let toc = document.getElementById('toc'),
+	song = document.getElementById('song'),
+	back = document.getElementById('back'),
+	modal = document.getElementById('overlay'),
+	modalClose = modal.querySelector('.close'),
+	modalBody = document.getElementById('modalBody');
 
-(function() {
-	var $toc = $(document.getElementById('toc')),
-		$song = $(document.getElementById('song')),
-		$back = $(document.getElementById('back')),
-		$modal = $(document.getElementById('overlay')),
-		$modalClose = $modal.find('.close'),
-		$modalBody = $(document.getElementById('modalBody'));
+let navigation = [],
+	songArray = [];
 
-	let navigation = []; 
-	let songArray = [];
+/* Start Point */
+populateNav();
+loadJSON(data); //Data is assigned from songs.js
 
-	populateNav();
-	loadJSON(data);
+//Hide modal on Esc
+document.addEventListener('keyup', (e) => {
+	if (e.which == 27) modalClose.click();
+});
 
-	//Hide modal on Esc
-	$(document).keyup(function(e) {
-		if (e.which == 27) $modalClose.click();
+modal.addEventListener('click', async () => {
+	await (() => {
+		return new Promise((resolve) => {
+			resolve(modal.classList.remove('active'));
+		});
+	})();
+	await delay(300);
+	await (() => {
+		return new Promise((resolve) => {
+			resolve(modal.setAttribute('hidden', ''));
+		});
+	})();
+});
+
+modalClose.addEventListener('click', async () => {
+	await (() => {
+		return new Promise((resolve) => {
+			resolve(modal.classList.remove('active'));
+		});
+	})();
+	await delay(300);
+	await (() => {
+		return new Promise((resolve) => {
+			resolve(modal.setAttribute('hidden', ''));
+		});
+	})();
+});
+
+back.addEventListener('click', async () => {
+	toc.removeAttribute('hidden');
+	song.setAttribute('hidden', '');
+	back.setAttribute('disabled', '');
+});
+
+/**
+ * Returns a delayed promise
+ */
+function delay(ms) {
+	return new Promise((resolve) => {
+		setTimeout(resolve, ms);
 	});
+}
 
-	$modal.on("click", function() {
-		$modal.removeClass('active').delay(300).fadeToggle(200);
-	});
-
-	$modalClose.on("click", function() {
-		$modal.removeClass('active').delay(300).fadeToggle(200);
-	});
-
-	$back.on("click", function() {
-		$toc.show();
-		$song.hide();
-		$back.attr('disabled', '');
-	});
-
-	//Add click listeners to spans in contents
-	function populateNav() {
-		navigation = $toc.find('span');
-		(function () {
-			for (var i = 0; i < navigation.length; i++) {
-				$(navigation[i]).on('click', function() {
-					var filter = $(this).text();
-					showRecords(filter.replace(/-/g, " "));
-				});
-			}
-		})();
+/**
+ *	Adds click listeners to spans in contents
+ */
+function populateNav() {
+	navigation = toc.querySelectorAll('span');
+	for (let i = 0; i < navigation.length; i++) {
+		navigation[i].addEventListener('click', function() {
+			let filter = this.innerText;
+			showRecords(filter.replace(/-/g, ' '));
+		});
 	}
+}
 
-	function showRecords(filter) {
-		var newFilter = filter.split(" ");
-		while ($modalBody[0].firstChild) {
-			$modalBody[0].removeChild($modalBody[0].firstChild);
+/**
+ * Displays songs depending on filter
+ */
+function showRecords(filter) {
+	let newFilter = filter.split(' ');
+	while (modalBody.firstChild) {
+		modalBody.removeChild(modalBody.firstChild);
+	}
+	if (newFilter === 'All') {
+		let allFilter = [['1 443']].join();
+		showRecords(allFilter);
+	} else if (newFilter.length > 1) {
+		//Display song titles by numbers
+		for (let i = 0; i < newFilter[1]; i++) {
+			let item = document.createElement('div');
+			let number = document.createElement('span');
+			number.classList += 'num';
+			number.innerText = i + 1;
+			item.appendChild(number);
+			item.onclick = (function(j) {
+				return function() {
+					showSong(j);
+				};
+			})(i);
+			item.innerText = songArray.songs.song[i].title;
+			modalBody.append(item);
+			item.appendChild(number);
 		}
-		if (newFilter[0] === "All") {
-			var allFilter = [["1 443"]].join();
-			showRecords(allFilter);
-		} else if (newFilter.length > 1) {
-			//Display song titles by numbers
-			for (var i = 0; i < newFilter[1]; i++) {
-				var item = document.createElement('div');
-				var number = document.createElement('span');
-				number.classList += "num";
-				number.innerText = i+1;
-				item.appendChild(number);
-				item.onclick = function(j)	{
-					return function() {
-						showSong(j);
-					}
-				}(i);
-				item.innerText = songArray.songs.song[i].title;
-				$modalBody.append(item);
-				item.appendChild(number);
+		modal.removeAttribute('hidden');
+		modal.classList.add('active');
+	} else {
+		let length = Object.keys(songArray.songs).length;
+		let array = [];
+		//Display song titles by letter
+		for (let i = 0; i < length; i++) {
+			if (songArray.songs[i].title.charAt(0) === newFilter[0]) {
+				array.push([songArray.songs[i].title, i]);
 			}
-			$modal.toggle().addClass("active");
-		} else {
-			var length = Object.keys(songArray.songs).length;
-			var array = [];
-			//Display song titles by letter
-			for (var i = 0; i < length; i++) {
-				if (songArray.songs[i].title.charAt(0) === newFilter[0]) {
-					array.push([songArray.songs[i].title, i]);
+		}
+		array.sort(function(a, b) {
+			return a[0].toUpperCase().localeCompare(b[0].toUpperCase());
+		});
+
+		for (let i = 0; i < array.length; i++) {
+			let item = document.createElement('div');
+			let number = document.createElement('span');
+			number.classList += 'num';
+			number.innerText = array[i][1] + 1;
+			item.onclick = (function(j) {
+				return function() {
+					showSong(array[j][1]);
+				};
+			})(i);
+			item.innerText = array[i][0];
+			modalBody.append(item);
+			item.appendChild(number);
+		}
+		modal.removeAttribute('hidden');
+		modal.classList.add('active');
+	}
+}
+
+/**
+ * Creates DOM layout for song details
+ */
+function showSong(index) {
+	let songInfo = songArray.songs[index];
+	let songStruct = [];
+	let orderSong = [];
+	let number = songInfo.number;
+	let title = songInfo.title;
+	let verses = songInfo.verse;
+	let chorus = songInfo.chorus;
+	let author = songInfo.author;
+
+	song.innerHTML = '';
+	songStruct.push(number, title, verses, chorus, author);
+
+	let ssLength = songStruct.length;
+	let header = document.createElement('div');
+	header.classList.add('songHeader');
+
+	for (let i = 0; i < ssLength; i++) {
+		switch (i) {
+			case 0:
+				if (songStruct[i]) {
+					let newDiv = document.createElement('div');
+					newDiv.classList.add('songNum');
+					newDiv.innerText = songStruct[i];
+					header.insertAdjacentElement('beforeend', newDiv);
 				}
-			}
-			array.sort(function(a,b) {
-				return a[0].toUpperCase().localeCompare(b[0].toUpperCase());
-			});
-
-		
-			for (var i = 0; i < array.length; i++) {
-				var item = document.createElement('div');
-				var number = document.createElement('span');
-				number.classList += "num";
-				number.innerText = array[i][1] + 1;
-				item.onclick = function(j) {
-					return function() {
-						showSong(array[j][1]);
-					};
-				}(i);
-				item.innerText = array[i][0];
-				$modalBody.append(item);
-				item.appendChild(number);
-			}
-			$modal.toggle().addClass("active");
-		}
-	}
-
-	function showSong(index) {
-		var song = songArray.songs[index];
-		var songStruct = [], orderSong = [];
-		var number = song.number;
-		var title = song.title;
-		var verses = song.verse;
-		var chorus = song.chorus;
-		var author = song.author;
-
-		$song[0].innerHTML = "";
-		songStruct.push(number, title, verses, chorus, author);
-		
-		var ssLength = songStruct.length;
-		var header = document.createElement("div");
-			header.classList += "songHeader";
-
-		for (var i = 0; i < ssLength; i++) {
-			switch (i) {
-				case 0:
-					if (songStruct[i]) {
-						var newDiv = document.createElement("div");
-						newDiv.classList += "songNum";
-						newDiv.innerText = songStruct[i];
-						header.insertAdjacentElement("beforeend", newDiv);
-					}
-					break;
-				case 1:
-					if (songStruct[i]) {
-						var newDiv = document.createElement("div");
-						newDiv.classList += "songTitle";
-						newDiv.innerText = songStruct[i];
-						header.insertAdjacentElement("afterbegin", newDiv);
-						$song.append(header);
-					}
-					break;
-				case 2:
-					if (songStruct[i]) {
-						var songLength = songStruct[i].length + 1;
-						for (var j = 0; j < songLength; j++) {
-							var newDiv = document.createElement("div");
-							switch (j) {
-								case 0: 
-									newDiv.classList += "verse";
-									newDiv.innerText = cleanupString(songStruct[i][j]);
-									$song.append(newDiv);
-									break;
-								case 1: 
+				break;
+			case 1:
+				if (songStruct[i]) {
+					let newDiv = document.createElement('div');
+					newDiv.classList.add('songTitle');
+					newDiv.innerText = songStruct[i];
+					header.insertAdjacentElement('afterbegin', newDiv);
+					song.insertAdjacentElement('beforeend', header);
+				}
+				break;
+			case 2:
+				if (songStruct[i]) {
+					let songLength = songStruct[i].length + 1;
+					for (let j = 0; j < songLength; j++) {
+						let newDiv = document.createElement('div');
+						switch (j) {
+							case 0:
+								newDiv.classList += 'verse';
+								newDiv.innerText = cleanupString(songStruct[i][j]);
+								song.insertAdjacentElement('beforeend', newDiv);
+								break;
+							case 1:
 								if (songStruct[3].length > 0) {
-										newDiv.classList += "chorus";
-										newDiv.innerText = cleanupString(songStruct[3]);
-										$song.append(newDiv);
-									}
-									break;
-								default:
-									newDiv.classList += "verse";
-									newDiv.innerText = cleanupString(songStruct[i][(j-1)]);
-									$song.append(newDiv);
-									break;
-							}
+									newDiv.classList += 'chorus';
+									newDiv.innerText = cleanupString(songStruct[3]);
+									song.insertAdjacentElement('beforeend', newDiv);
+								}
+								break;
+							default:
+								newDiv.classList += 'verse';
+								newDiv.innerText = cleanupString(songStruct[i][j - 1]);
+								song.insertAdjacentElement('beforeend', newDiv);
+								break;
 						}
 					}
-					break;
-				case 4:
-					if (songStruct[i]) {
-						var newDiv = document.createElement("div");
-						newDiv.classList += "author";
-						newDiv.innerText = songStruct[i];
-						$song.append(newDiv);
-					}
-					break;
-			}
+				}
+				break;
+			case 4:
+				if (songStruct[i]) {
+					let newDiv = document.createElement('div');
+					newDiv.classList += 'author';
+					newDiv.innerText = songStruct[i];
+					song.insertAdjacentElement('beforeend', newDiv);
+				}
+				break;
 		}
-
-		$toc.hide();
-		$song.show();
-		$back.attr('disabled', false);
-		$modalClose.click();
-
-		return false;
 	}
 
-	//Remove **, excess space from string
-	function cleanupString(string) {
-		var newString = string.replace(/\*/g, "").replace(/\s{2,}/g, "\n").trim();
-		return newString;
-	}
+	toc.setAttribute('hidden', '');
+	song.removeAttribute('hidden');
+	back.removeAttribute('disabled');
+	modalClose.click();
 
-	function isEmpty(str) {
-		return (!str || 0 === str.length);
-	}
+	return false;
+}
 
-	function loadJSON(file) {
-		console.log(file);
-		if (localStorage.getItem("songs") === null) {
-			console.log("Item Not Found!");
-			localStorage.setItem("songs", JSON.stringify(file));
-			console.log("Item Set!");
-		}
-		songArray = JSON.parse(JSON.stringify(file));
-		//console.log(Object.keys(songArray.songs.song[0]).length);
+/**
+ * Remove dirty characters, spaces from string
+ */
+function cleanupString(string) {
+	let newString = string
+		.replace(/\*/g, '')
+		.replace(/\s{2,}/g, '\n')
+		.trim();
+	return newString;
+}
+
+/**
+ * Checks for empty string
+ */
+function isEmpty(str) {
+	return !str || 0 === str.length;
+}
+
+/**
+ * Loads JSON info into local storage and populates songArray
+ */
+function loadJSON(file) {
+	if (localStorage.getItem('songs') === null) {
+		localStorage.setItem('songs', JSON.stringify(file));
 	}
-})();
+	songArray = JSON.parse(localStorage.getItem('songs'));
+}
