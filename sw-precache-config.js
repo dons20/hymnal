3a239eb738f672c942656b60c044506bd1dfe23b
Keno Clayton <keno.clayton15@gmail.com>
2020-03-25 23:07:00 -0500

    SW precache config change

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,14 +1,14 @@
 module.exports = {
-	staticFileGlobs: [
-		'build/static/css/**.css',
-		'build/static/js/**.js',
-		'build/**.js',
-		'build/**.png',
-		'build/**.svg',
-		'build/songs.json'
-	],
-	swFilePath: './build/service-worker-local.js',
-	stripPrefix: 'build/',
-	importScripts: ['./service-worker-local.js'],
-	handleFetch: false
+    staticFileGlobs: [
+        "build/static/css/**.css",
+        "build/static/js/**.js",
+        "build/static/media/**.svg",
+        "build/songs.json",
+        "build/**.js",
+        "build/**.png"
+    ],
+    swFilePath: "./build/service-worker-local.js",
+    stripPrefix: "build/",
+    importScripts: ["./service-worker-local.js"],
+    handleFetch: false
 };
