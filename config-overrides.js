4c906b21387e0a8604a71203e1dc2df8b80dfd7b
Keno Clayton <keno.clayton15@gmail.com>
2020-07-12 09:46:00 -0500

    Updated service worker code

--- config-overrides.js
+++ config-overrides.js
@@ -2,41 +2,19 @@ const { override, useBabelRc, adjustWorkbox } = require("customize-cra");
 const { InjectManifest, GenerateSW } = require("workbox-webpack-plugin");
 const path = require("path");
 
-const webpack = function (config, env) {
+const webpack = function (config) {
 	config.plugins.push(
 		new InjectManifest({
-			globPatterns: ["./rainbow/*.jpg"],
-			globDirectory: "build",
 			swSrc: path.join("public", "service-worker-local.js"),
-			swDest: "service-worker-local.js",
-			include: [/\.html$/],
+		}),
+		new GenerateSW({
+			clientsClaim: true,
+			exclude: (config.exclude || []).concat("index.html"),
+			navigateFallback: "public/" + "index.html",
 		})
-		/* new GenerateSW({
-            clientsClaim: true,
-            exclude: [/\.map$/, /asset-manifest\.json$/],
-            importWorkboxFrom: "cdn",
-            navigateFallback: "public/" + "index.html",
-            navigateFallbackBlacklist: [
-                // Exclude URLs starting with /_, as they're likely an API call
-                new RegExp("^/_")
-                // Exclude any URLs whose last part seems to be a file extension
-                // as they're likely a resource and not a SPA route.
-                // URLs containing a "?" character won't be blacklisted as they're likely
-                // a route with query params (e.g. auth callbacks).
-                //new RegExp('/[^/?]+\\.[^/]+$'),
-            ]
-        }) */
 	);
 
 	return config;
 };
 
-module.exports = override(
-	useBabelRc(),
-	adjustWorkbox(wb => {
-		Object.assign(wb, {
-			skipWaiting: true,
-			exclude: (wb.exclude || []).concat("index.html"),
-		});
-	})
-);
+module.exports = override(useBabelRc(), adjustWorkbox(webpack));
--- public/service-worker-local.js
+++ public/service-worker-local.js
@@ -1,34 +1,38 @@
-console.log("Service Worker code goes here");
 var CACHE_NAME = "my-pwa-cache-v1";
 var urlsToCache = [
-    "./",
-    "./songs",
-    "./songs.json",
-    "./favicon.ico",
-    "./manifest.json",
-    "./launcher-144.png",
-    "./launcher-192.png",
-    "./launcher-512.png",
-    "./service-worker-local.js"
+	"/",
+	"/index.html",
+	"/songs.json",
+	"/favicon.ico",
+	"/manifest.json",
+	"/launcher-144.png",
+	"/launcher-192.png",
+	"/launcher-512.png",
 ];
 
 //Cache Files on first load
-self.addEventListener("install", function(e) {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(function(cache) {
-            // Open a cache and cache our files
-            return cache.addAll(urlsToCache);
-        })
-    );
+self.addEventListener("install", function (e) {
+	// @ts-ignore
+	e.waitUntil(
+		caches.open(CACHE_NAME).then(function (cache) {
+			// Open a cache and cache our files
+			return cache.addAll(urlsToCache);
+		})
+	);
 });
 
 //Serve cached data
-self.addEventListener("fetch", function(e) {
-    console.log(e.request.url);
-    if (e.request.cache === "only-if-cached" && e.request.mode !== "same-origin") return;
-    e.respondWith(
-        caches.match(e.request).then(function(response) {
-            return response || fetch(e.request);
-        })
-    );
+self.addEventListener("fetch", function (e) {
+	// @ts-ignore
+	console.log(e.request.url);
+	// @ts-ignore
+	if (e.request.cache === "only-if-cached" && e.request.mode !== "same-origin") return;
+	// @ts-ignore
+	e.respondWith(
+		// @ts-ignore
+		caches.match(e.request).then(function (response) {
+			// @ts-ignore
+			return response || fetch(e.request);
+		})
+	);
 });
