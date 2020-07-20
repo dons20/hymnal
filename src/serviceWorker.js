ed516c555b2b4ae7cd49d5bef272f42e23a0e5b9
Keno Clayton <keno.clayton15@gmail.com>
2020-07-20 04:07:59 -0500

    another test

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -8,18 +8,18 @@
 // resources are updated in the background.
 
 // To learn more about the benefits of this model and instructions on how to
-// opt-in, read http://bit.ly/CRA-PWA
+// opt-in, read https://cra.link/PWA
 
 const isLocalhost = Boolean(
 	window.location.hostname === "localhost" ||
 		// [::1] is the IPv6 localhost address.
 		window.location.hostname === "[::1]" ||
-		// 127.0.0.1/8 is considered localhost for IPv4.
+		// 127.0.0.0/8 are considered localhost for IPv4.
 		window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
 );
 
 export function register(config) {
-	if ("serviceWorker" in navigator) {
+	if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator) {
 		// The URL constructor is available in all browsers that support SW.
 		const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
 		if (publicUrl.origin !== window.location.origin) {
@@ -30,7 +30,7 @@ export function register(config) {
 		}
 
 		window.addEventListener("load", () => {
-			const swUrl = `${process.env.PUBLIC_URL}/service-worker-local.js`;
+			const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
 			if (isLocalhost) {
 				// This is running on localhost. Let's check if a service worker still exists or not.
@@ -39,9 +39,9 @@ export function register(config) {
 				// Add some additional logging to localhost, pointing developers to the
 				// service worker/PWA documentation.
 				navigator.serviceWorker.ready.then(() => {
-					console.info(
+					console.log(
 						"This web app is being served cache-first by a service " +
-							"worker. To learn more, visit http://bit.ly/CRA-PWA"
+							"worker. To learn more, visit https://cra.link/PWA"
 					);
 				});
 			} else {
@@ -69,7 +69,7 @@ function registerValidSW(swUrl, config) {
 							// content until all client tabs are closed.
 							console.log(
 								"New content is available and will be used when all " +
-									"tabs for this page are closed. See http://bit.ly/CRA-PWA."
+									"tabs for this page are closed. See https://cra.link/PWA."
 							);
 
 							// Execute callback
@@ -123,8 +123,12 @@ function checkValidServiceWorker(swUrl, config) {
 
 export function unregister() {
 	if ("serviceWorker" in navigator) {
-		navigator.serviceWorker.ready.then(registration => {
-			registration.unregister();
-		});
+		navigator.serviceWorker.ready
+			.then(registration => {
+				registration.unregister();
+			})
+			.catch(error => {
+				console.error(error.message);
+			});
 	}
 }
